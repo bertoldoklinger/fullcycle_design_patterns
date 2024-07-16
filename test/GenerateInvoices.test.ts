063054a9a39c862aa4127104eda105e9f43700cd
Bertoldo Klinger <bertoldokbrj@gmail.com>
2024-07-16 13:35:42 -0300

    feat: Generate invoices based on input parameters
    The code changes in this commit add functionality to generate invoices based on input parameters. The `GenerateInvoices` class now accepts an `Input` object with properties for the month, year, and type of invoice generation (accrual or cash). The `execute` method has been updated to use the input parameters to query the database and generate the corresponding invoices.

--- test/GenerateInvoices.test.ts
+++ test/GenerateInvoices.test.ts
@@ -1,7 +1,46 @@
-import GenerateInvoices from "../src/GenerateInvoices"
+import GenerateInvoices, { Input } from "../src/GenerateInvoices"
 
 
-test('Deve gerar as notas fiscais', async () => {
+test('Deve gerar as notas fiscais por regime de caixa', async () => {
   const generateInvoices = new GenerateInvoices()
-  await generateInvoices.execute()
+  const input: Input = {
+    month: 1,
+    year: 2022,
+    type: "cash"
+  }
+
+  const output = await generateInvoices.execute(input)
+
+  expect(output.at(0)?.date).toBe("2022-01-05")
+  expect(output.at(0)?.amount).toBe(6000)
+  
+})
+
+
+test('Deve gerar as notas fiscais por regime de competência do mês 01', async () => {
+  const generateInvoices = new GenerateInvoices()
+  const input:Input = {
+    month: 1,
+    year: 2022,
+    type: "accrual"
+  }
+
+  const output = await generateInvoices.execute(input)
+
+  expect(output.at(0)?.date).toBe("2022-01-01")
+  expect(output.at(0)?.amount).toBe(500)
+})
+
+test('Deve gerar as notas fiscais por regime de competência do mês 2', async () => {
+  const generateInvoices = new GenerateInvoices()
+  const input:Input = {
+    month: 2,
+    year: 2022,
+    type: "accrual"
+  }
+
+  const output = await generateInvoices.execute(input)
+
+  expect(output.at(0)?.date).toBe("2022-02-01")
+  expect(output.at(0)?.amount).toBe(500)
 })
